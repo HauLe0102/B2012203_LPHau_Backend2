2d6e224211fe3dced50329bd2931d7124f6670e0
Hau Le 02 <haulp0210@gmail.com>
2024-09-19 12:21:35 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route");
-//const ApiError = require("./app/api-error");
+const ApiError = require("./app/api-error");
 
 const app = express();
 
@@ -15,14 +15,14 @@ app.get("/", (req, res) => {
     })
 });
 
-//app.use((req, res, next) => {
-//    return next(new ApiError(404, "Resource not found"));
-//});
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
 
-//app.use((error, req, res, next) => {
- //   return res.status(error.statusCode || 500).json({
- //       message: error.message || "Internal Server Error",
- //   });
-//});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
+});
 
 module.exports = app
