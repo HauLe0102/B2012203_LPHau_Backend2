4cb18975346f5679f869f2b1ad794f917a5c43ba
Hau Le 02 <haulp0210@gmail.com>
2024-09-19 12:17:05 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-//const contactRouter = require("./app/routes/contact.route");
+const contactRouter = require("./app/routes/contact.route");
 //const ApiError = require("./app/api-error");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-//app.use("/api/contacts/", contactRouter);
+app.use("/api/contacts/", contactRouter);
 
 app.get("/", (req, res) => {
     res.json({
